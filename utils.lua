7bb8fcc11d840c2aef6062864bbf630c8a4f36a8
mk8 <mk8.bruh@gmail.com>
2024-12-23 18:34:27 +0100

    utils typo

--- utils.lua
+++ utils.lua
@@ -32,7 +32,7 @@ sqit.utils.stretchOut(elems, direction, from, to, spaceAround) - stretch the ele
 return {
     merge = function(a, b)
         for k, v in pairs(b) do
-            a[k] = b
+            a[k] = v
         end
         return a
     end,
